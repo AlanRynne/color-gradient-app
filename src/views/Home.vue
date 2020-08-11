c4772397e8e852c0488648fe89939a5367c6830e
Alan Rynne <alan@rynne.es>
2020-08-11 15:31:08 +0200

    Added hover effect to css

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -43,8 +43,8 @@ import {Component, Vue} from 'vue-property-decorator'
 import ColorGradient from "@/components/ColorGradient.vue"
 
 function wait(seconds: number) {
-  return new Promise((resolve)=> {
-    setTimeout(resolve,seconds)
+  return new Promise((resolve) => {
+    setTimeout(resolve, seconds)
   })
 }
 
@@ -53,7 +53,7 @@ function wait(seconds: number) {
 })
 export default class Home extends Vue {
 
-  private colorMap = null
+  private colorMap = (t: number) => '#ffffff'
   private circleCount = 24
   private positionX = 0.5
   private positionY = 0
@@ -73,6 +73,16 @@ export default class Home extends Vue {
     window.removeEventListener('resize', this.handleResize)
   }
 
+  mouseOverCircle(e: MouseEvent) {
+    console.log('over circle', e.target)
+    const t = e.target as HTMLElement
+      t.setAttribute('opacity', '0.2')
+  }
+  mouseLeaveCircle(e: MouseEvent){
+    const t = e.target as HTMLElement
+    t.setAttribute('opacity', '1')
+
+  }
   ComputeCircleCount() {
     const cnt = this.container
     if (!cnt) return 25
@@ -154,11 +164,21 @@ export default class Home extends Vue {
   background-color: transparent !important;
 }
 
-.loading-overlay.custom-loading{
+.loading-overlay.custom-loading {
   .loading-background {
     background-color: #ff9d00;
     background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='100%25' height='100%25' viewBox='0 0 1600 800'%3E%3Cg stroke='%23000' stroke-width='66.7' stroke-opacity='0.11' %3E%3Ccircle fill='%23ff9d00' cx='0' cy='0' r='1800'/%3E%3Ccircle fill='%23f27d00' cx='0' cy='0' r='1700'/%3E%3Ccircle fill='%23e55f00' cx='0' cy='0' r='1600'/%3E%3Ccircle fill='%23d84400' cx='0' cy='0' r='1500'/%3E%3Ccircle fill='%23cb2c00' cx='0' cy='0' r='1400'/%3E%3Ccircle fill='%23bf1600' cx='0' cy='0' r='1300'/%3E%3Ccircle fill='%23b20300' cx='0' cy='0' r='1200'/%3E%3Ccircle fill='%23a5000e' cx='0' cy='0' r='1100'/%3E%3Ccircle fill='%2398001c' cx='0' cy='0' r='1000'/%3E%3Ccircle fill='%238b0027' cx='0' cy='0' r='900'/%3E%3Ccircle fill='%237e0030' cx='0' cy='0' r='800'/%3E%3Ccircle fill='%23710037' cx='0' cy='0' r='700'/%3E%3Ccircle fill='%2364003b' cx='0' cy='0' r='600'/%3E%3Ccircle fill='%2358003c' cx='0' cy='0' r='500'/%3E%3Ccircle fill='%234b003a' cx='0' cy='0' r='400'/%3E%3Ccircle fill='%233e0037' cx='0' cy='0' r='300'/%3E%3Ccircle fill='%23310030' cx='0' cy='0' r='200'/%3E%3Ccircle fill='%23210024' cx='0' cy='0' r='100'/%3E%3C/g%3E%3C/svg%3E");
     background-attachment: fixed;
-    background-size: cover;  }
+    background-size: cover;
+  }
+}
+
+circle {
+  transition: all 2s !important;
+
+  &:hover {
+    transition: all .3s ease-out !important;
+    opacity: 0;
+  }
 }
 </style>
